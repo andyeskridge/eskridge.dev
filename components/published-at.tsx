3f7ce62be980320fd6cb1a5844a50d2977b212e7
Andy Eskridge <andy.eskridge@gmail.com>
2019-10-17 12:49:58 -0500

    fix date formating

--- components/published-at.tsx
+++ components/published-at.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Link from "next/link";
-import { parse, format, fromUnixTime } from "date-fns";
+import { parse, format } from "date-fns";
 
-function PublishedAt({ link, date }: { link: string; date: number }) {
+function PublishedAt({ link, date }: { link: string; date: string }) {
   return (
     <>
       <Link href={link}>
         <a href={link} className="u-url">
-          <time className="dt-published">{date}</time>
+          <time className="dt-published">
+            {format(parse(date, "yyyy-MM-dd", Date.now()), "MMMM dd, yyyy")}
+          </time>
         </a>
       </Link>
       <style jsx>{`
