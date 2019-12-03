d73a391df3c5080287bd3d4de4d4321aad38136b
Andy Eskridge <aeskridge@cenergistic.com>
2019-12-02 22:50:29 -0600

    static was deprecated for public folder now

--- components/head.tsx
+++ components/head.tsx
@@ -22,7 +22,7 @@ export default ({
     <title>{title ? `${title} - ${siteMeta.title}` : siteMeta.title}</title>
     <meta name="description" content={description} />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" href="/static/favicon.ico" />
+    <link rel="icon" href="/favicon.ico" />
 
     <link
       rel="alternate"
