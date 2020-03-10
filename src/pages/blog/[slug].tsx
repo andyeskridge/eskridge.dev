4befef16de76047f548a2af50f85f512f5cbcd3f
Andy Eskridge <aeskridge@cenergistic.com>
2020-03-10 12:24:17 -0500

    cleanup

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Head from 'next/head'
 import Header from '../../components/header'
 import Heading from '../../components/heading'
 import components from '../../components/dynamic'
