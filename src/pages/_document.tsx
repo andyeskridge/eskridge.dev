77924e0b6ec5cb0cc938676f4de107a320038853
Andy Eskridge <andy.eskridge@gmail.com>
2020-03-20 12:51:35 -0500

    only support dark-mode for right now

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head />
-        <body className="bg-gray-200 dark-mode:bg-dark dark-mode:text-gray-100">
+        <body className="bg-dark text-gray-100">
           <Main />
           <NextScript />
         </body>
