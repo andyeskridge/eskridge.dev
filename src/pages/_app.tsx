bc051475e9602c62d0e71956a8cade30de0e3a60
Andy Eskridge <andy.eskridge@gmail.com>
2020-04-17 00:05:44 -0500

    add fathom

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '../styles/tailwind.css'
-import { AppPropsType } from 'next/dist/next-server/lib/utils'
+import React, { useEffect } from "react";
+import Router from "next/router";
+import { AppPropsType } from "next/dist/next-server/lib/utils";
 
-export default ({ Component, pageProps }: AppPropsType) => (
-  <Component {...pageProps} />
-)
+import * as Fathom from "fathom-client";
+import "../styles/tailwind.css";
+
+// Record a pageview when route changes
+Router.events.on("routeChangeComplete", () => {
+  Fathom.trackPageview();
+});
+
+export default ({ Component, pageProps }: AppPropsType) => {
+  // Initialize Fathom when the app loads
+  useEffect(() => {
+    if (process.env.NODE_ENV === "production") {
+      Fathom.load();
+      Fathom.setSiteId("FNASISBH");
+      Fathom.trackPageview();
+    }
+  }, []);
+  return <Component {...pageProps} />;
+};
