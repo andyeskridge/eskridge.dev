85e59620d6bfb02cc47d6cbdf0e95ca27ad158ca
Andy Eskridge <andy.eskridge@gmail.com>
2020-05-11 17:05:58 -0500

    add fathom tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,15 @@
+import React, { useEffect } from "react";
 import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    let tracker = window.document.createElement("script");
+    let firstScript = window.document.getElementsByTagName("script")[0];
+    tracker.defer = true;
+    tracker.setAttribute("site", "FNASISBH");
+    tracker.setAttribute("spa", "auto");
+    tracker.src = "https://ecoonll.eskridge.dev/script.js";
+    firstScript.parentNode.insertBefore(tracker, firstScript);
+  }, []);
   return <Component {...pageProps} />;
 }
