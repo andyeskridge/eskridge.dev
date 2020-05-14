643c316a10e5b9868c2b450064a5fb556165c805
Andy Eskridge <andy.eskridge@gmail.com>
2020-05-14 13:05:41 -0500

    fix link

--- components/cover-image.tsx
+++ components/cover-image.tsx
@@ -31,7 +31,7 @@ export default function CoverImage({
   return (
     <div className="-mx-5 sm:mx-0">
       {slug ? (
-        <Link as={`/posts/${slug}`} href="/posts/[slug]">
+        <Link href={`/posts/${slug}`}>
           <a aria-label={title}>{image}</a>
         </Link>
       ) : (
