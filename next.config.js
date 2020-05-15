8101412b932bea8441f58e9a63c96904ae0ffd15
Andy Eskridge <andy.eskridge@gmail.com>
2020-05-15 11:49:00 -0500

    remove default config

--- next.config.js
+++ next.config.js
@@ -7,29 +7,5 @@ const optimizedImages = require("next-optimized-images");
 module.exports = withMDX(
   optimizedImages({
     pageExtensions: ["ts", "tsx", "md", "mdx"],
-    inlineImageLimit: 8192,
-    imagesFolder: "images",
-    imagesName: "[name]-[hash].[ext]",
-    handleImages: ["jpeg", "jpg", "png", "svg", "webp", "gif"],
-    optimizeImages: true,
-    optimizeImagesInDev: false,
-    mozjpeg: {
-      quality: 80,
-    },
-    optipng: {
-      optimizationLevel: 3,
-    },
-    pngquant: false,
-    gifsicle: {
-      interlaced: true,
-      optimizationLevel: 3,
-    },
-    svgo: {
-      // enable/disable svgo plugins here
-    },
-    webp: {
-      preset: "default",
-      quality: 75,
-    },
   })
 );
