f6a7c94fbfd81501157bc5ca2282bb448bb5353c
Andy Eskridge <andy.eskridge@gmail.com>
2020-05-15 14:29:50 -0500

    add sitemap and robots.txt

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
+const withPlugins = require("next-compose-plugins");
 const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
 });
-
 const optimizedImages = require("next-optimized-images");
 
-module.exports = withMDX(
-  optimizedImages({
-    pageExtensions: ["ts", "tsx", "md", "mdx"],
-  })
-);
+module.exports = withPlugins([withMDX, optimizedImages], {
+  pageExtensions: ["ts", "tsx", "md", "mdx"],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require("./lib/generate-sitemap");
+    }
+
+    return config;
+  },
+});
