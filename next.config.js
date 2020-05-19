fb14d0215cf609e2fbda7ddfaf984a0f2c75cb83
Andy Eskridge <andy.eskridge@gmail.com>
2020-05-19 09:59:47 -0500

    remove image optimization loaders that aren't being used right now. also switch to sharp from jimp

--- next.config.js
+++ next.config.js
@@ -4,13 +4,22 @@ const withMDX = require("@next/mdx")({
 });
 const optimizedImages = require("next-optimized-images");
 
-module.exports = withPlugins([withMDX, optimizedImages], {
-  pageExtensions: ["ts", "tsx", "md", "mdx"],
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      require("./lib/generate-sitemap");
-    }
+module.exports = withPlugins(
+  [
+    withMDX,
+    [
+      optimizedImages,
+      { responsive: { adapter: require("responsive-loader/sharp") } },
+    ],
+  ],
+  {
+    pageExtensions: ["ts", "tsx", "md", "mdx"],
+    webpack: (config, { isServer }) => {
+      if (isServer) {
+        require("./lib/generate-sitemap");
+      }
 
-    return config;
-  },
-});
+      return config;
+    },
+  }
+);
