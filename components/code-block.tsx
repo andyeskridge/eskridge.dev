f69a610ed904db4d7b0691f06119fcd5d706ce05
Andy Eskridge <andy.eskridge@gmail.com>
2020-05-19 15:33:38 -0500

    switch to inlined styles instead of module.css and update code-block to work

--- components/code-block.tsx
+++ components/code-block.tsx
@@ -1,9 +1,34 @@
 import React from "react";
 import Highlight, { defaultProps } from "prism-react-renderer";
-import { LiveProvider, LivePreview } from "react-live";
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
+//@ts-ignore
+import { mdx } from "@mdx-js/react";
+import theme from "prism-react-renderer/themes/vsDark";
 
 export default ({ children, className, live, render }) => {
   const language = className.replace(/language-/, "");
+  if (live) {
+    return (
+      <div className="overflow-hidden rounded-lg">
+        <LiveProvider
+          code={children}
+          transformCode={(code) => "/** @jsx mdx */" + `<>${code}</>`}
+          scope={{ mdx }}
+          theme={theme}
+        >
+          <LivePreview className="p-4 border-t border-l border-r rounded-t-lg" />
+          <LiveEditor
+            className="overflow-x-auto break-normal"
+            style={{
+              fontFamily:
+                'Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
+            }}
+          />
+          <LiveError />
+        </LiveProvider>
+      </div>
+    );
+  }
 
   if (render) {
     return (
@@ -16,7 +41,7 @@ export default ({ children, className, live, render }) => {
   }
 
   return (
-    <Highlight {...defaultProps} code={children.trim()} language={language}>
+    <Highlight {...defaultProps} code={children} language={language}>
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
         <pre className={className} style={{ ...style, padding: "20px" }}>
           {tokens.map((line, i) => (
--- components/post-body.tsx
+++ components/post-body.tsx
@@ -1,9 +1,27 @@
-import markdownStyles from "./markdown-styles.module.css";
+import { MDXProvider } from "@mdx-js/react";
+import codeBlock from "./code-block";
+
+const components = {
+  p: (props) => <p className="my-6" {...props} />,
+  ul: (props) => <ul className="my-6" {...props} />,
+  ol: (props) => <ol className="my-6" {...props} />,
+  blockquote: (props) => <blockquote className="my-6" {...props} />,
+  a: (props) => <a className="underline hover:text-blue-500" {...props} />,
+  h1: (props) => <h1 className="text-4xl mt-16 mb-4 leading-snug" {...props} />,
+  h2: (props) => <h2 className="text-3xl mt-12 mb-4 leading-snug" {...props} />,
+  h3: (props) => <h2 className="text-2xl mt-8 mb-4 leading-snug" {...props} />,
+  pre: (props) => <div {...props} />,
+  code: codeBlock,
+};
 
 export default function PostBody({ children }) {
   return (
-    <div className="max-w-2xl mx-auto">
-      <div className={markdownStyles["markdown"]}>{children}</div>
+    <div className="max-w-2xl mx-auto text-lg leading-relaxed">
+      <MDXProvider components={components}>
+        <>
+          <div>{children}</div>
+        </>
+      </MDXProvider>
     </div>
   );
 }
