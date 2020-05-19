85e561c7cca613f95682e25ad640dc0e18338a66
Andy Eskridge <andy.eskridge@gmail.com>
2020-05-19 10:30:53 -0500

    working on sizing for post previews

--- components/cover-image.tsx
+++ components/cover-image.tsx
@@ -4,9 +4,16 @@ import Link from "next/link";
 export default function CoverImage({
   title,
   src,
+  small = false,
   slug = undefined,
   credit = undefined,
 }) {
+  const srcSet = small
+    ? require(`images/${src}?resize&sizes[]=640&sizes[]=332&sizes[]=428&sizes[]=556`)
+    : require(`images/${src}?resize&sizes[]=600&sizes[]=728&sizes[]=984&sizes[]=1240`);
+  const sizes = small
+    ? "(max-width: 640px) 640px, (max-width: 768px) 600px, (max-width: 1024px) 332px, (max-width: 1280px) 428px, 556px"
+    : "(max-width: 640px) 600px, (max-width: 768px) 728px, (max-width: 1024px) 984px, 1240px";
   const image = (
     <div className="relative">
       <img
@@ -20,11 +27,8 @@ export default function CoverImage({
         className={cn("shadow-small absolute top-0 left-0", {
           "hover:shadow-medium transition-shadow duration-200": slug,
         })}
-        srcSet={
-          require(`images/${src}?resize&sizes[]=600&sizes[]=728&sizes[]=984&sizes[]=1240`)
-            .srcSet
-        }
-        sizes="(max-width: 640px) 600px, (max-width: 768px) 728px, (max-width: 1024px) 984px, 1240px,"
+        srcSet={srcSet.srcSet}
+        sizes={sizes}
         loading="lazy"
         alt={title}
       />
--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -19,6 +19,7 @@ export default function PostPreview({
           title={title}
           src={coverImage.url}
           credit={coverImage.credit}
+          small
         />
       </div>
       <h3 className="text-3xl mb-3 leading-snug">
