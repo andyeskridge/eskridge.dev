31f6a4137ff6859b7088178bdc20cac3e7a30834
Andy Eskridge <andy.eskridge@gmail.com>
2020-05-19 14:49:13 -0500

    add sorting by date to posts

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 import { join } from "path";
+import { parseISO, compareDesc } from "date-fns";
 
 const DIR = join(process.cwd(), "./pages/posts/");
 const files = fs
@@ -7,19 +8,21 @@ const files = fs
   .filter((file) => file.endsWith(".md") || file.endsWith(".mdx"));
 
 export const getAllPosts = () => {
-  return files.map((file) => {
-    const META = /export\s+const\s+meta\s+=\s+(\{(\n|[^])*?\n\})/;
-    const name = join(DIR, file);
-    const contents = fs.readFileSync(name, "utf8");
-    const match = META.exec(contents);
-    if (!match || typeof match[1] !== "string")
-      throw new Error(`${name} needs to export const meta = {}`);
+  return files
+    .map((file) => {
+      const META = /export\s+const\s+meta\s+=\s+(\{(\n|[^])*?\n\})/;
+      const name = join(DIR, file);
+      const contents = fs.readFileSync(name, "utf8");
+      const match = META.exec(contents);
+      if (!match || typeof match[1] !== "string")
+        throw new Error(`${name} needs to export const meta = {}`);
 
-    const meta = eval("(" + match[1] + ")");
+      const meta = eval("(" + match[1] + ")");
 
-    return {
-      ...meta,
-      path: "/posts/" + file.replace(/\.mdx?$/, ""),
-    };
-  });
+      return {
+        ...meta,
+        path: "/posts/" + file.replace(/\.mdx?$/, ""),
+      };
+    })
+    .sort((a, b) => compareDesc(parseISO(a.date), parseISO(b.date)));
 };
