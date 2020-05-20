c01e7c6f8e824e499a9c4de7dcc003e70f60572f
Andy Eskridge <andy.eskridge@gmail.com>
2020-05-19 21:49:06 -0500

    remove the trace and use the new img heights for consistency

--- components/cover-image.tsx
+++ components/cover-image.tsx
@@ -15,24 +15,17 @@ export default function CoverImage({
     ? "(max-width: 640px) 640px, (max-width: 768px) 600px, (max-width: 1024px) 332px, (max-width: 1280px) 428px, 556px"
     : "(max-width: 640px) 600px, (max-width: 768px) 728px, (max-width: 1024px) 984px, 1240px";
   const image = (
-    <div className="relative">
-      <img
-        className={cn("shadow-small", {
-          "hover:shadow-medium transition-shadow duration-200": slug,
-        })}
-        src={require(`images/${src}?trace`).trace}
-        alt={`SVG trace of ${title}`}
-      />
-      <img
-        className={cn("shadow-small absolute top-0 left-0", {
-          "hover:shadow-medium transition-shadow duration-200": slug,
-        })}
-        srcSet={srcSet.srcSet}
-        sizes={sizes}
-        loading="lazy"
-        alt={title}
-      />
-    </div>
+    <img
+      className={cn("shadow-small object-cover w-full", {
+        "hover:shadow-medium transition-shadow duration-200": slug,
+        "sm:height-320 md:height-300 lg:height-166 xl:height-278": small,
+        "sm:height-300 md:height-364 lg:height-492 xl:height-620": !small,
+      })}
+      srcSet={srcSet.srcSet}
+      sizes={sizes}
+      loading="lazy"
+      alt={title}
+    />
   );
   return (
     <div className="-mx-5 sm:mx-0">
