79cb0b316bc8b0674ec0ef967cc4cddecec6b390
Andy Eskridge <andy.eskridge@gmail.com>
2021-01-01 23:36:58 -0600

    more cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
 import '../styles/index.css'
 import { AppProps } from 'next/app'
+import React, { useEffect } from "react";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    let firstScript = window.document.getElementsByTagName("script")[0];
+
+    let preconnect = window.document.createElement("link");
+    preconnect.rel = "preconnect";
+    preconnect.href = "https://ecoonll.eskridge.dev";
+    firstScript.parentNode?.insertBefore(preconnect, firstScript);
+
+    let tracker = window.document.createElement("script");
+    tracker.defer = true;
+    tracker.setAttribute("site", "FNASISBH");
+    tracker.setAttribute("spa", "auto");
+    tracker.src = "https://ecoonll.eskridge.dev/script.js";
+    firstScript.parentNode?.insertBefore(tracker, firstScript);
+  }, []);
+  return <Component {...pageProps} />;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,6 @@ import Intro from '../components/intro'
 import Layout from '../components/layout'
 import { getAllPosts } from '../lib/api'
 import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
 import PostType from '../types/post'
 
 export default function Index({ allPosts }: { allPosts: PostType[]}) {
@@ -15,7 +14,7 @@ export default function Index({ allPosts }: { allPosts: PostType[]}) {
     <>
       <Layout>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>eskridge.dev</title>
         </Head>
         <Container>
           <Intro />
