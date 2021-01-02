ad4254d3c756ccf23b8e842a9bfdb433b7d38972
Andy Eskridge <andy.eskridge@gmail.com>
2021-01-01 22:49:39 -0600

    remove alert message

--- components/alert.tsx
+++ components/alert.tsx
@@ -1,13 +1,11 @@
 import Container from "./container";
 import cn from "classnames";
-import { EXAMPLE_PATH } from "../lib/constants";
 
 export default function Alert({ preview }: { preview?: boolean }) {
   return (
     <div
-      className={cn("border-b", {
-        "bg-accent-7 border-accent-7 text-white": preview,
-        "bg-accent-1 border-accent-2": !preview,
+      className={cn({
+        "border-b bg-accent-7 border-accent-7 text-white": preview,
       })}
     >
       <Container>
@@ -25,14 +23,6 @@ export default function Alert({ preview }: { preview?: boolean }) {
             </>
           ) : (
             <>
-              The source code for this blog is{" "}
-              <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-                className="underline hover:text-success duration-200 transition-colors"
-              >
-                available on GitHub
-              </a>
-              .
             </>
           )}
         </div>
